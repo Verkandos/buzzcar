fec27e529c2acda51cd1a96d25782c7135764416
2FAuth0 <jcruz2705@protonmail.com>
2025-09-14 17:47:12 -0400

    Create headers and main

---
+++ include/GPIOManager.hpp
@@ -0,0 +1,25 @@
+// Photosensor Left
+#define PHOTO_SENSOR_LEFT_PIN 4
+
+// Photosensor Right
+#define PHOTO_SENSOR_RIGHT_PIN 7
+
+// Audio 
+#define AUDIO_PIN 9
+
+// LCD (TBD)
+#define LCD_MOSI_PIN 10
+#define LCD_SCCLK_PIN 11
+#define LCD_CS_PIN 12
+#define LCD_DC_RS_PIN 13
+#define LCD_RST_PIN 14
+
+// Motor A
+#define MOTOR_A_PIN 15
+
+// Motor B
+#define MOTOR_B_PIN 17
+
+// Reserved - USB-UART (Leave free for flashing/serial)
+#define USB_UART_TX_PIN 20
+#define USB_UART_RX_PIN 21
