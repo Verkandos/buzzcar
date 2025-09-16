3434112c753d00056b128e4608afbb7ae270f7f8
2FAuth0 <jcruz2705@protonmail.com>
2025-09-16 01:58:07 -0400

    Updated GPIO Mapping

--- include/GPIOManager.hpp
+++ include/GPIOManager.hpp
@@ -4,40 +4,51 @@
 
 // GPIO Pin Definitions for the project
 
-// Photosensor Left (Analog)
-#define PHOTO_SENSOR_LEFT_PIN 4
+// Photosensor A (Analog)
+#define PHOTO_SENSOR_A_PIN 15
 
-// Photosensor Right (Analog)
-#define PHOTO_SENSOR_RIGHT_PIN 7
+// Photosensor B (Analog)
+#define PHOTO_SENSOR_B_PIN 23
+
+// Photosensor C (Analog)
+#define PHOTO_SENSOR_C_PIN 22
 
 // Audio (Output PWM)
-#define AUDIO_PIN 9
+#define AUDIO_PIN 5
 
 // LCD (TBD)
-#define LCD_MOSI_PIN 10
-#define LCD_SCCLK_PIN 11
-#define LCD_CS_PIN 12
-#define LCD_DC_RS_PIN 13
-#define LCD_RST_PIN 14
+#define LCD_MOSI_PIN 6
+#define LCD_SCCLK_PIN 7
+#define LCD_CS_PIN 10
+#define LCD_DC_RS_PIN 11
+#define LCD_RST_PIN 2
 
 // Motor A (PWM Output)
-#define MOTOR_A_PIN 15
+#define MOTOR_A_PIN 21
 
 // Motor B
-#define MOTOR_B_PIN 17
+#define MOTOR_B_PIN 20
 
-// Reserved - USB-UART (Leave free for flashing/serial)
-#define USB_UART_TX_PIN 20
-#define USB_UART_RX_PIN 21
 
 class GPIOManager {
     private:
         std::map<int, std::string> pinMappings;
-    public:
+        std::map<int, int> pwmChannels;
+        
+        // Singleton pattern - private constructor and members
         GPIOManager(); 
+        
+        // Prevent copying
+        GPIOManager(const GPIOManager&) = delete;
+        GPIOManager& operator=(const GPIOManager&) = delete;
+        
+    public:
+        // Static method to get the singleton instance
+        static GPIOManager& getInstance();
 
         void initializePins(const std::map<int, std::string>& mappings);
         void configurePin(int pin, const std::string& mode);
+        void configurePWMPin(int pin, int frequency = 1000, int resolution = 8);
         void writePWM(int pin, int duty);
         void writeDigital(int pin, bool value);
         int readAnalog(int pin);
