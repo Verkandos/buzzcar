9800201616aae24a17b9a906bd9169fb41a8e93a
2FAuth0 <jcruz2705@protonmail.com>
2025-10-24 11:21:28 -0400

    Moved GPIO Pin Definitions to ControlConfig.hpp

--- include/GPIOManager.hpp
+++ include/GPIOManager.hpp
@@ -2,38 +2,7 @@
 #include <map>
 #include <string>
 
-// GPIO Pin Definitions for the project
-// ===========INPUTS===========
-
-// Photosensor A (Input Analog: ADC Pin)
-#define PHOTO_SENSOR_A_PIN 3 // [X]
-
-// Photosensor B (Input Analog: ADC Pin)
-#define PHOTO_SENSOR_B_PIN 2 // [X]
-
-// Photosensor C (Input Analog: ADC Pin)
-#define PHOTO_SENSOR_C_PIN 1 // [X]
-
-// User Button (Input Digital: GPIO Pin)
-#define USER_BUTTON_PIN 11 // [X] 
-
-// ===========OUTPUTS==========
-
-// Audio (Output [PWM]: GPIO Pin)
-#define AUDIO_PIN 23 // [X]
-
-// LCD (GPIO Pin)
-#define LCD_DATA_PIN 22 // [X]
-#define LCD_CLK_PIN 21 // [X]
-
-
-// Motor A (Output [PWM]: GPIO Pin)
-#define MOTOR_A_PIN 20 // [X]
-
-// Motor B (Output [PWM]: GPIO Pin)
-#define MOTOR_B_PIN 19 // [X]
-
-
+// GPIO Pin Definitions moved to ControlConfig.hpp
 
 class GPIOManager {
     private:
